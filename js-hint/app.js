29c467ed4534e5957ad266c7cc51b4663e2adf05
Edynilson Silva <edynilson-silva-desktop@users.noreply.github.com>
2020-02-02 18:27:49 -0300

    Uso do JS Hint

---
+++ js-hint/app.js
@@ -0,0 +1,12 @@
+/* npm install jshint -g */
+
+/* Para utilizar jshint NomeArquivo 
+  Adicionar configurações personalizadas, criar um arquivo config.json
+  e chamar no terminal jshint NomeArquivo --config ./config.json
+*/
+
+function main(a, b) {
+  if (b) {
+    return a === null;
+  }
+}
